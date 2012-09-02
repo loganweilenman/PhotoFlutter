8a6dbbdb37231b79d9b402237d1a6d74d01b0886
loganweilenman <loganweilenman@gmail.com>
2012-09-02 10:05:59 -0400

    fixed errors

--- PhotoFlutter/Global.asax.cs
+++ PhotoFlutter/Global.asax.cs
@@ -23,7 +23,7 @@ namespace PhotoFlutter
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
