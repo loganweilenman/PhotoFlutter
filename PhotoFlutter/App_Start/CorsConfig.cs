ff01c53f5c2f002f0ff35e5bad6b3d5c4ebdbb15
loganweilenman <loganweilenman@gmail.com>
2012-09-04 04:38:07 -0400

    added cors class

---
+++ PhotoFlutter/App_Start/CorsConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Thinktecture.IdentityModel.Http.Cors.WebApi;
+
+namespace PhotoFlutter
+{
+    public class CorsConfig
+    {
+        public static void RegisterCors(HttpConfiguration httpConfig)
+
+        {
+            WebApiCorsConfiguration corsConfig = new WebApiCorsConfiguration();
+
+            // this adds the CorsMessageHandler to the HttpConfiguration’s
+
+            // MessageHandlers collection
+
+            corsConfig.RegisterGlobal(httpConfig);
+
+       
+
+            // this allow all CORS requests to the Products controller
+
+            // from the http://foo.com origin.
+
+            corsConfig
+
+                .ForResources("Products")
+
+                .ForOrigins("http://jsfiddle.net")
+
+                .AllowAll();
+
+        }
+    }
+}
